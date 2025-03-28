686fd26fa9a49e5fcab9208e5ff5907624fd8f6f
unknown <alexey90fed@gmail.com>
2025-03-28 19:41:54 +0200

    SpringBoot

---
+++ src/main/java/org/example/SpringBootDomashka/SpringBootDomashkaApplication.java
@@ -0,0 +1,13 @@
+package org.example.SpringBootDomashka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDomashkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDomashkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/SpringBootDomashka/SpringBootDomashkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.SpringBootDomashka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDomashkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
